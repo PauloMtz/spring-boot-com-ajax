5ec7e0e114dcb648eee52d619e06f24338ed4820
PauloMtz <pramosmtz@gmail.com>
2022-10-15 20:43:58 -0300

    Carregando os dados no console - classe controller e função ajax

---
+++ src/main/resources/static/js/promo-add.js
@@ -0,0 +1,14 @@
+$("#linkPromocao").on("change", function() {
+    var url = $(this).val();
+
+    if (url.length > 7) {
+        $.ajax({
+            method: "POST",
+            url: "/meta/info?url=" + url,
+            cache: false,
+            success: function(dados) {
+                console.log(dados);
+            }
+        })
+    }
+})

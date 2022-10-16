18b98c1cf08ef332e37725a9161c77b42b8b3746
PauloMtz <pramosmtz@gmail.com>
2022-10-16 09:16:11 -0300

    Capturando erros e exibindo alerta

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -11,6 +11,17 @@ $("#linkPromocao").on("change", function() {
                 $("#titulo").val(dados.title);
                 $("#site").text(dados.site.replace("@", ""));
                 $("#linkImagem").attr("src", dados.image);
+            },
+            statusCode: {
+                404: function() {
+                    $("#alert").addClass("alert alert-danger").text("Erro 404: Nenhuma informação retornada.");
+                },
+                500: function() {
+                    $("#alert").addClass("alert alert-danger").text("Erro 500: Nenhuma informação retornada.");
+                },
+                error: function() {
+                    $("#alert").addClass("alert alert-danger").text("Error: Nenhuma informação retornada.");
+                }
             }
         })
     }

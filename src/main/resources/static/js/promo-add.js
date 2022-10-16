dd45f67cf10e2dcae7bebf2501e23b55b9e663f5
PauloMtz <pramosmtz@gmail.com>
2022-10-16 10:08:31 -0300

    Incluindo loader na imagem do formulário

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -10,23 +10,31 @@ $("#linkPromocao").on("change", function() {
                 $("#alert").removeClass("alert alert-danger").text("");
                 $("#titulo").val("");
                 $("#site").text("");
-                $("#linkImagem").attr("src", "/images/promo-dark.png");
+                $("#linkImagem").attr("src", "");
+                $("#loader-img").addClass("loader");
             },
             success: function(dados) {
                 console.log(dados);
                 $("#titulo").val(dados.title);
                 $("#site").text(dados.site.replace("@", ""));
+                $("#loader-img").removeClass("loader");
                 $("#linkImagem").attr("src", dados.image);
             },
             statusCode: {
                 404: function() {
                     $("#alert").addClass("alert alert-danger").text("Erro 404: Nenhuma informação retornada.");
+                    $("#loader-img").removeClass("loader");
+                    $("#linkImagem").attr("src", "/images/promo-dark.png");
                 },
                 500: function() {
                     $("#alert").addClass("alert alert-danger").text("Erro 500: Nenhuma informação retornada.");
+                    $("#loader-img").removeClass("loader");
+                    $("#linkImagem").attr("src", "/images/promo-dark.png");
                 },
                 error: function() {
                     $("#alert").addClass("alert alert-danger").text("Error: Nenhuma informação retornada.");
+                    $("#loader-img").removeClass("loader");
+                    $("#linkImagem").attr("src", "/images/promo-dark.png");
                 }
             }
         })

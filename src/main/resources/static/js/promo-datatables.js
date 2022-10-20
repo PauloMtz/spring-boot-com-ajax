01ebf9661816c16e23ac35fa8578e84c7c6ce752
PauloMtz <pramosmtz@gmail.com>
2022-10-20 06:38:44 -0300

    Datatable - preparando o lado cliente

---
+++ src/main/resources/static/js/promo-datatables.js
@@ -0,0 +1,24 @@
+$(documen).ready(function() {
+    $("#table-server").DataTable({
+        processing: true,
+        serverSide: true,
+        responsive: true,
+        lengthMenu: [5, 10, 15, 20],
+        ajax: {
+            url: "/promocao/datatable/server",
+            data: "data"
+        },
+        columns: [
+            {data: 'id'},
+            {data: 'titulo'},
+            {data: 'site'},
+            {data: 'linkPromocao'},
+            {data: 'descricao'},
+            {data: 'linkImagem'},
+            {data: 'preco'},
+            {data: 'likes'},
+            {data: 'dataCadastro'},
+            {data: 'categoria.titulo'},
+        ]
+    });
+});
